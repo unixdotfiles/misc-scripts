80d75547e11a7e4299bf27481e51d31743804408
Eitan Adler <lists@eitanadler.com>
2016-12-16 20:17:42 -0800

    clone bin with submodules

--- recover-state-clone.sh
+++ recover-state-clone.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 cd ~
-git clone git@github.com:unixdotfiles/bin
+git clone --recurse-submodules git@github.com:unixdotfiles/bin
 git clone git@github.com:unixdotfiles/dotfiles .conf
 
 mkdir -p ~/exps
