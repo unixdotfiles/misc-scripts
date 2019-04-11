332ec03cede3d2808b1454bd281db021753c9471
Eitan Adler <lists@eitanadler.com>
2019-04-11 00:30:19 -0700

    Add all.sh for showing off poly.html

---
+++ fun/all.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+# html
+# Javascript
+python2 poly.html
+clang++ -w -x c poly.html && ./a.out
+clang++ -w -trigraphs -x c poly.html && ./a.out
+clang -w -x c++ poly.html && ./a.out
+clang -w -trigraphs -x c++ poly.html && ./a.out
+perl poly.html
+perl6 poly.html
+sh poly.html
+zsh poly.html
+bash poly.html
+ruby poly.html
+make -f poly.html
+tclsh poly.html
+wsc poly.html
+ghc -w -x lhs poly.html 2>/dev/null && ./poly
+bf2c < poly.html
+
+rm poly a.out
