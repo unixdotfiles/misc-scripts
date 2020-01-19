eb231f108b2e6236c2bf33fa92984471637268c9
Eitan Adler <lists@eitanadler.com>
2020-01-19 10:41:39 -0800

    wakeup-latency: add stats

--- debugging-utils/wakeup-latency/src/main.rs
+++ debugging-utils/wakeup-latency/src/main.rs
@@ -22,17 +22,26 @@ use std::time::{Instant, Duration};
 use libc::usleep;
 use floating_duration::TimeAsFloat;
 
+use stats::OnlineStats;
+
+const SECOND: Duration = Duration::from_secs(1);
+
 fn main() {
-    let second: Duration = Duration::new(1, 0);
+    let mut stat_container = OnlineStats::new();
+//    signal_hook::flag::register(signal_hook::SIGINFO, display_stats);
     loop {
         let before: Instant = Instant::now();
 
         unsafe {
-            let _ = usleep(second.as_micros() as _);
+            let _ = usleep(SECOND.as_micros() as _);
         }
         let delta: Duration = before.elapsed();
-        let diff_from_second = delta - second;
-        println!("{:?}", diff_from_second.as_fractional_secs());
+        let diff_from_second = delta - SECOND;
+        stat_container.add(diff_from_second.as_nanos());
+        println!("{} (mean: {} stddev: {})",
+            diff_from_second.as_fractional_secs(),
+            stat_container.mean() / SECOND.as_nanos() as f64,
+            stat_container.stddev() / SECOND.as_nanos() as f64
+        );
     }
-
 }
