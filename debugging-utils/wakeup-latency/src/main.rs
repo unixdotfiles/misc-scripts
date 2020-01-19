121fd524c13492da736811a09d25da430921e4ee
Eitan Adler <lists@eitanadler.com>
2020-01-19 10:20:10 -0800

    wakeup-latency: be a bit more strict
    This is overkill for this project but its a good intro for me to learn
    how to be stricter.

--- debugging-utils/wakeup-latency/src/main.rs
+++ debugging-utils/wakeup-latency/src/main.rs
@@ -1,25 +1,37 @@
 #![deny(warnings)]
 #![deny(missing_docs)]
+#![deny(anonymous_parameters)]
+#![deny(bare_trait_objects)]
+#![deny(elided_lifetimes_in_paths)]
+#![deny(missing_copy_implementations)]
+#![deny(missing_debug_implementations)]
+#![deny(single_use_lifetimes)]
+#![deny(trivial_casts)]
+#![deny(trivial_numeric_casts)]
+#![deny(unreachable_pub)]
+#![deny(unused_extern_crates)]
+#![deny(unused_import_braces)]
+#![deny(unused_qualifications)]
+#![deny(unused_results)]
+#![deny(variant_size_differences)]
 
 //! Shows how much latency usleep has
 
 use std::time::{Instant, Duration};
 
 use libc::usleep;
-use floating_duration::{TimeAsFloat};
+use floating_duration::TimeAsFloat;
 
 fn main() {
-    let mut all_deltas: Vec<Duration> = Vec::new();
     let second: Duration = Duration::new(1, 0);
     loop {
         let before: Instant = Instant::now();
 
         unsafe {
-            usleep(second.as_micros() as _);
+            let _ = usleep(second.as_micros() as _);
         }
         let delta: Duration = before.elapsed();
         let diff_from_second = delta - second;
-        all_deltas.push(diff_from_second);
         println!("{:?}", diff_from_second.as_fractional_secs());
     }
 
